f3393474916d444b38aa2a9af51936a3ce10d2de
leonelmota <leonelmota5@gmail.com>
2022-02-05 12:37:35 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -37,6 +37,16 @@ public class Rental {
         return thisAmount;
    }
 
+   public int getFrequentRenterPoints(){
+        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            this.getDaysRented() > 1){
+            return 2;
+        }
+        else{
+            return 1;
+        }
+   }
+
      public static void main(String[] args){
       
   }
